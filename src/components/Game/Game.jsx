1d7a1f089c3da5cb7f3b77ffef0f1b75cc765dd7
2020-DEV-006 <you@example.com>
2019-11-23 20:10:18 +0530

    Handle player win

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -2,44 +2,131 @@ import React, { Component } from "react";
 import ScoreBoard from "../scoreBoard/scoreBoard";
 import Player from "../player/player";
 
-const points = [0, 15, 30, 40, "won"];
+const Tennis = {
+  DEUCE: "Deuce!",
+  WON: "Won!",
+  PLAYER: "Player",
+  
+};
 
+const points = [0, 15, 30, 40, "won"];
 export default class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
       firstPlayerPointIndex: 0,
-      secondPlayerPointIndex: 0
+      secondPlayerPointIndex: 0,
+      gamePoint: false,
+      firstPlayerAdvantage: false,
+      secondPlayerAdvantage: false,
+      won: 0,
+      deuce: false
     };
   }
-  updateScorePlayer = player => {
-    var { firstPlayerPointIndex, secondPlayerPointIndex } = this.state;
 
-    const { playerPointIndex, opponentPlayerPointIndex } =
+ 
+
+updateScorePlayer = player => {
+    var {
+      deuce,
+      gamePoint,
+      firstPlayerAdvantage,
+      secondPlayerAdvantage,
+      firstPlayerPointIndex,
+      secondPlayerPointIndex,
+      won
+    } = this.state;
+
+    const {
+      playerPointIndex,
+      opponentPlayerPointIndex,
+      playerAdvantage,
+      opponentPlayerAdvantage
+    } =
       player === 1
         ? {
             playerPointIndex: firstPlayerPointIndex + 1,
-            opponentPlayerPointIndex: secondPlayerPointIndex
+            opponentPlayerPointIndex: secondPlayerPointIndex,
+            playerAdvantage: firstPlayerAdvantage,
+            opponentPlayerAdvantage: secondPlayerAdvantage
           }
         : {
             playerPointIndex: secondPlayerPointIndex + 1,
-            opponentPlayerPointIndex: firstPlayerPointIndex
+            opponentPlayerPointIndex: firstPlayerPointIndex,
+            playerAdvantage: secondPlayerAdvantage,
+            opponentPlayerAdvantage: firstPlayerAdvantage
           };
-          
+
+    //player update score logic
+    if (!won) {
+      if (gamePoint && !opponentPlayerAdvantage) {
+        this.setState({ won: player });
+      }
+      if (deuce) {
+        if (playerAdvantage) {
+          this.setState({ gamePoint: true });
+        } else {
+          this.setState({
+            secondPlayerAdvantage: player !== 1,
+            firstPlayerAdvantage: player === 1
+          });
+        }
+      } else {
+        const pointIndex =
+          player === 1
+            ? {
+                firstPlayerPointIndex: playerPointIndex
+              }
+            : {
+                secondPlayerPointIndex: playerPointIndex
+              };
+
+        if (points[playerPointIndex] === "won") {
+          this.setState({ won: player });
+        } else if (
+          points[playerPointIndex] === 40 &&
+          points[opponentPlayerPointIndex] === 40
+        ) {
+          this.setState({
+            deuce: true,
+            ...pointIndex
+          });
+        } else {
+          this.setState({ ...pointIndex });
+        }
+      }
+      if (opponentPlayerAdvantage) {
+        this.setState({
+          deuce: true,
+          firstPlayerPointIndex: 3,
+          secondPlayerPointIndex: 3,
+          firstPlayerAdvantage: false,
+          secondPlayerAdvantage: false
+        });
+      }
+    }
   };
+
   render() {
     const {
-        firstPlayerPointIndex,
-        secondPlayerPointIndex
-       
-      } = this.state;
+      firstPlayerPointIndex,
+      secondPlayerPointIndex,
+      won,
+      deuce,
+      secondPlayerAdvantage,
+      firstPlayerAdvantage
+    } = this.state;
+
     return (
       <div className="tennisBackground">
         <h2 className="tennisHeading">Play Tennis</h2>
-        <ScoreBoard
-          firstPlayer={points[firstPlayerPointIndex]}
-          secondPlayer={points[secondPlayerPointIndex]}
-        />
+        <div>
+          
+          <ScoreBoard
+            firstPlayer={points[firstPlayerPointIndex]}
+            secondPlayer={points[secondPlayerPointIndex]}
+          />
+        </div>
         <div className="playerBox">
           <Player
             playerName="Player 1"
@@ -52,6 +139,8 @@ export default class Game extends Component {
             updateScore={this.updateScorePlayer.bind(this)}
           />
         </div>
+       
+        {won && <h1>PLAYER {won} WON !!!</h1>}
       </div>
     );
   }
--- src/components/Game/Game.test.jsx
+++ src/components/Game/Game.test.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Game from "./Game";
-
+import { shallow } from "enzyme";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import Player from '../Player/Player';
+import ScoreBoard from "../scoreBoard/scoreBoard";
 
 
 configure({ adapter: new Adapter() });
@@ -14,4 +16,27 @@ describe("Game", () => {
     ReactDOM.render(<Game />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
+  it("handle the win scenario", () => {
+    const updateScore = jest.fn();
+    const points = [0, 15, 30, 40, "won"];
+    const button = shallow(<Player updateScore={updateScore} />);
+    var score = shallow(<ScoreBoard />);
+    var status = 0;
+    points.map((data, id) => {
+      if (id > 0) {
+        button
+          .find(".player")
+          .props()
+          .onClick();
+        status = points[id];
+      }
+    });
+    status = status == "won" ? 1 : 0;
+    expect(updateScore).toHaveBeenCalledTimes(4);
+    score.setProps({ firstPlayer: status });
+    expect(score.props().children[1].props.children[1].props.children).toEqual(
+      1
+    );
+  });
+
 });
