c20d60d1eeb3e40a0b2690c862930eb19f0fde8a
2020-DEV-006 <you@example.com>
2019-11-23 19:41:57 +0530

    Handle players first ball

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -2,15 +2,55 @@ import React, { Component } from "react";
 import ScoreBoard from "../scoreBoard/scoreBoard";
 import Player from "../player/player";
 
+const points = [0, 15, 30, 40, "won"];
+
 export default class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstPlayerPointIndex: 0,
+      secondPlayerPointIndex: 0
+    };
+  }
+  updateScorePlayer = player => {
+    var { firstPlayerPointIndex, secondPlayerPointIndex } = this.state;
+
+    const { playerPointIndex, opponentPlayerPointIndex } =
+      player === 1
+        ? {
+            playerPointIndex: firstPlayerPointIndex + 1,
+            opponentPlayerPointIndex: secondPlayerPointIndex
+          }
+        : {
+            playerPointIndex: secondPlayerPointIndex + 1,
+            opponentPlayerPointIndex: firstPlayerPointIndex
+          };
+          
+  };
   render() {
+    const {
+        firstPlayerPointIndex,
+        secondPlayerPointIndex
+       
+      } = this.state;
     return (
       <div className="tennisBackground">
         <h2 className="tennisHeading">Play Tennis</h2>
-        <ScoreBoard />
+        <ScoreBoard
+          firstPlayer={points[firstPlayerPointIndex]}
+          secondPlayer={points[secondPlayerPointIndex]}
+        />
         <div className="playerBox">
-          <Player playerName="Player 1" playerNumber={1} />
-          <Player playerName="Player 2" playerNumber={2} />
+          <Player
+            playerName="Player 1"
+            playerNumber={1}
+            updateScore={this.updateScorePlayer.bind(this)}
+          />
+          <Player
+            playerName="Player 2"
+            playerNumber={2}
+            updateScore={this.updateScorePlayer.bind(this)}
+          />
         </div>
       </div>
     );
--- src/components/player/player.test.jsx
+++ src/components/player/player.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Player from './player';
 import { configure } from 'enzyme';
+import { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
@@ -13,4 +14,11 @@ describe('Player', () => {
         ReactDOM.render(<Player />, div);
         ReactDOM.unmountComponentAtNode(div);
     });
+    it('handle players ball call "updateScore" prop on button click', () => {
+        const updateScore = jest.fn();
+        const button = shallow((<Player updateScore={updateScore} />));
+        button.find('.player').props().onClick();
+        expect(updateScore).toHaveBeenCalled();
+        expect(updateScore).toHaveBeenCalledTimes(1);
+      });
 });
